f0b031e3751b00f152fff7a4d6bedbadfb89d764
Arun Babu Neelicattu <abn@redhat.com>
2013-05-07 01:09:33 +1000

    Update VictimsCLI.java

--- src/main/java/com/redhat/victims/VictimsCLI.java
+++ src/main/java/com/redhat/victims/VictimsCLI.java
@@ -3,16 +3,10 @@ package com.redhat.victims;
 import java.io.IOException;
 
 public class VictimsCLI {
-
-	/**
- * 	 * @param args
- * 	 	 * @throws IOException
- * 	 	 	 */
 	public static void main(String[] args) throws IOException {
 		for (String arg : args) {
 			VictimsScanner.scan(arg, System.out);
 		}
 	}
-
 }
 
